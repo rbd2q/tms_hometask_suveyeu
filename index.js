39ef3ab45f1a8a717017a4621a3d6c790f02815d
rbd2q <you@example.comvania-suveev@mail.ru>
2021-07-18 17:20:22 +0300

    Modified html and js files

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const requestURL = 'https://api.thecatapi.com/v1/images/search';
+const image = document.createElement('img');
+const container = document.getElementById('container');
 
+
+async function sendRequest() {
+    try {
+        const response = await fetch(requestURL);
+        const picture = await response.json();
+        const url = picture[0].url;
+        container.append(image);
+        image.src = url;
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+sendRequest();
+
+document.addEventListener('click', () => {
+    sendRequest();
+})
